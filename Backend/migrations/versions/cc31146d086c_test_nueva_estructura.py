2c034f74eaf2e4d2202d41b03093fd06f30a2f86
David Salamanca <d.salamancaz@uniandes.edu.co>
2024-10-18 23:38:43 -0500

    Carga a Master

--- Backend/migrations/versions/cc31146d086c_test_nueva_estructura.py
+++ Backend/migrations/versions/cc31146d086c_test_nueva_estructura.py
@@ -1,8 +1,8 @@
 """Test nueva estructura
 
-Revision ID: 97df5a9ef0bf
+Revision ID: cc31146d086c
 Revises: 
-Create Date: 2024-10-18 22:10:24.454976
+Create Date: 2024-10-18 23:32:27.327095
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '97df5a9ef0bf'
+revision = 'cc31146d086c'
 down_revision = None
 branch_labels = None
 depends_on = None
