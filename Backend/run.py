391ff355ca027ebc83660757f05d8348a3f379f7
David Salamanca <d.salamancaz@uniandes.edu.co>
2024-11-03 13:32:05 -0500

    Despliegue Ajustes de Seguridad

--- Backend/run.py
+++ Backend/run.py
@@ -1,8 +1,8 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
