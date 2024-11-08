76dbe7428635fe55f9917a5b249e34ba1912f3be
David Salamanca <d.salamancaz@uniandes.edu.co>
2024-11-08 15:00:13 -0500

    Despliegue GraphQL

--- Backend/app/models/schema.py
+++ Backend/app/models/schema.py
@@ -22,17 +22,33 @@ class IncidentType:
     created_date: str
     modified_date: str
     source: str
-    customer_id: str
-    analyst_id: str
+    #customer_id: str
+    #analyst_id: str
     status: str
+    logs: List['IncidentLogType']
+
+    @strawberry.field
+    def customer(self) -> UserType:
+        user = db.session.query(Users).filter(Users.id == self.customer_id).first()
+        return UserType(id=user.id, username=user.username, email=user.email, phone=user.phone, name=user.name, last_name=user.last_name, role=user.role)
+    
+    # @strawberry.field
+    # def analyst(self) -> UserType:
+    #     user = db.session.query(Users).filter(Users.id == self.analyst_id).first()
+    #     return UserType(id=user.id, username=user.username, email=user.email, phone=user.phone, name=user.name, last_name=user.last_name, role=user.role)
+
 
 @strawberry.type
 class IncidentLogType:
     id: str
     details: str
     created_date: str
-    users_id: str
     incident_id: str
+
+    @strawberry.field
+    def user(self) -> UserType:
+        user = db.session.query(Users).filter(Users.id == self.users_id).first()
+        return UserType(id=user.id, username=user.username, email=user.email, phone=user.phone, name=user.name, last_name=user.last_name, role=user.role)
 
 @strawberry.type
 class ContractType:
@@ -48,6 +64,8 @@ class CompanyType:
     id: str
     name: str
     created_date: str
+    users: List['UserType']
+    contracts: List['ContractType']
 
 @strawberry.type
 class RatesType:
@@ -87,40 +105,71 @@ class CompanyServicesType:
 @strawberry.type
 class Query:
     @strawberry.field
-    def users(self) -> List[UserType]:
-        return db.session.query(Users).all()
+    def users(self, id: str = None, username: str = None) -> List[UserType]:
+        query = db.session.query(Users)
+        if id:
+            query = query.filter(Users.id.like(f"%{id}%"))
+        if username:
+            query = query.filter(Users.username.like(f"%{username}%"))
+        return query.all()
 
     @strawberry.field
-    def incidents(self) -> List[IncidentType]:
-        return db.session.query(Incident).all()
+    def incidents(self,description: str = None) -> List[IncidentType]:
+        query = db.session.query(Incident)
+        if description:
+            query = query.filter(Incident.description.like(f"%{description}%"))
+        return query.all()
 
     @strawberry.field
-    def incident_logs(self) -> List[IncidentLogType]:
-        return db.session.query(IncidentLog).all()
+    def incident_logs(self, details: str = None) -> List[IncidentLogType]:
+        query = db.session.query(IncidentLog)
+        if details:
+            query = query.filter(IncidentLog.details.like(f"%{details}%"))
+        return query.all()
 
     @strawberry.field
-    def contracts(self) -> List[ContractType]:
-        return db.session.query(Contract).all()
+    def contracts(self, id: str = None, description: str = None) -> List[ContractType]:
+        query = db.session.query(Contract)
+        if id:
+            query = query.filter(Contract.id.like(f"%{id}%"))
+        if description:
+            query = query.filter(Contract.description.like(f"%{description}%"))
+        return query.all()
 
     @strawberry.field
-    def companies(self) -> List[CompanyType]:
-        return db.session.query(Company).all()
+    def companies(self, name: str = None) -> List[CompanyType]:
+        query = db.session.query(Company)
+        if name:
+            query = query.filter(Company.name.like(f"%{name}%"))
+        return query.all()
 
     @strawberry.field
-    def rates(self) -> List[RatesType]:
-        return db.session.query(Rates).all()
+    def rates(self, source: str = None) -> List[RatesType]:
+        query = db.session.query(Rates)
+        if source:
+            query = query.filter(Rates.source.like(f"%{source}%"))
+        return query.all()
 
     @strawberry.field
-    def invoices(self) -> List[InvoicesType]:
-        return db.session.query(Invoices).all()
+    def invoices(self, amount: float = None) -> List[InvoicesType]:
+        query = db.session.query(Invoices)
+        if amount:
+            query = query.filter(Invoices.amount == amount)
+        return query.all()
 
     @strawberry.field
-    def log_invoices(self) -> List[LogInvoicesType]:
-        return db.session.query(LogInvoices).all()
+    def log_invoices(self, description: str = None) -> List[LogInvoicesType]:
+        query = db.session.query(LogInvoices)
+        if description:
+            query = query.filter(LogInvoices.description.like(f"%{description}%"))
+        return query.all()
 
     @strawberry.field
-    def company_services(self) -> List[CompanyServicesType]:
-        return db.session.query(CompanyServices).all()
+    def company_services(self, description: str = None) -> List[CompanyServicesType]:
+        query = db.session.query(CompanyServices)
+        if description:
+            query = query.filter(CompanyServices.description.like(f"%{description}%"))
+        return query.all()
 
 # Definir el esquema de GraphQL
 schema = strawberry.Schema(query=Query)
