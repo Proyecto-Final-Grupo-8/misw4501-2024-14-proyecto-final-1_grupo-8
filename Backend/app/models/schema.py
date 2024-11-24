a8d8fcfcc6e60104a3ffeb7feb2895157a78c429
David Salamanca <d.salamancaz@uniandes.edu.co>
2024-11-23 23:14:15 -0500

    Despliegue Backend Graphql

--- Backend/app/models/schema.py
+++ Backend/app/models/schema.py
@@ -1,10 +1,17 @@
 import strawberry
 from typing import List, Optional
+from sqlalchemy.orm import joinedload
 from app.models import Users, Incident, IncidentLog, Contract, Company, Rates, Invoices, LogInvoices, CompanyServices
 from app.extensions import db
 
-# Definición de tipos de GraphQL
+# Función genérica para aplicar filtros dinámicos
+def apply_filters(query, model, filters: dict):
+    for key, value in filters.items():
+        if value is not None:
+            query = query.filter(getattr(model, key).like(f"%{value}%"))
+    return query
 
+# Definición de tipos de GraphQL
 @strawberry.type
 class UserType:
     id: str
@@ -22,8 +29,6 @@ class IncidentType:
     created_date: str
     modified_date: str
     source: str
-    #customer_id: str
-    #analyst_id: str
     status: str
     logs: List['IncidentLogType']
 
@@ -31,12 +36,6 @@ class IncidentType:
     def customer(self) -> UserType:
         user = db.session.query(Users).filter(Users.id == self.customer_id).first()
         return UserType(id=user.id, username=user.username, email=user.email, phone=user.phone, name=user.name, last_name=user.last_name, role=user.role)
-    
-    # @strawberry.field
-    # def analyst(self) -> UserType:
-    #     user = db.session.query(Users).filter(Users.id == self.analyst_id).first()
-    #     return UserType(id=user.id, username=user.username, email=user.email, phone=user.phone, name=user.name, last_name=user.last_name, role=user.role)
-
 
 @strawberry.type
 class IncidentLogType:
@@ -101,61 +100,113 @@ class CompanyServicesType:
     created_date: str
 
 # Definición de las consultas en GraphQL
-
 @strawberry.type
 class Query:
-    @strawberry.field 
-    def users(self, id: Optional[str] = None, username: Optional[str] = None, email: Optional[str] = None, phone: Optional[str] = None, name: Optional[str] = None, last_name: Optional[str] = None, role: Optional[str] = None ) -> List[UserType]: 
-        query = db.session.query(Users) 
-        filters = { 
-            "id": id, 
-            "username": username, 
-            "email": email, 
-            "phone": phone, 
-            "name": name, 
-            "last_name": last_name, 
-            "role": role 
-            } 
-        for key, value in filters.items(): 
-            if value is not None: 
-                query = query.filter(getattr(Users, key).like(f"%{value}%")) 
+    @strawberry.field
+    def users(self, id: Optional[str] = None, username: Optional[str] = None, email: Optional[str] = None, 
+              phone: Optional[str] = None, name: Optional[str] = None, last_name: Optional[str] = None, 
+              role: Optional[str] = None) -> List[UserType]:
+        filters = {
+            "id": id,
+            "username": username,
+            "email": email,
+            "phone": phone,
+            "name": name,
+            "last_name": last_name,
+            "role": role
+        }
+        query = db.session.query(Users)
+        query = apply_filters(query, Users, filters)
         return query.all()
 
     @strawberry.field
-    def incidents(self,description: Optional[str] = None, created_date: Optional[str] = None, modified_date: Optional[str] = None, source: Optional[str] = None, status: Optional[str] = None ) -> List[IncidentType]:
-        query = db.session.query(Incident)
-        filter = {
+    def incidents(self, 
+                  description: Optional[str] = None, 
+                  created_date: Optional[str] = None, 
+                  modified_date: Optional[str] = None, 
+                  source: Optional[str] = None, 
+                  status: Optional[str] = None, 
+                  customer_id: Optional[str] = None, 
+                  customer_username: Optional[str] = None, 
+                  customer_email: Optional[str] = None, 
+                  customer_name: Optional[str] = None,
+                  incident_logs_id: Optional[str] = None,
+                  incident_logs_details: Optional[str] = None,
+                  incident_logs_created_date: Optional[str] = None                  
+                  ) -> List[IncidentType]:
+        # Filtros para la tabla Incident
+        filters = {
             "description": description,
             "created_date": created_date,
             "modified_date": modified_date,
             "source": source,
             "status": status
         }
-        for key, value in filter.items():
-            if value:
-                query = query.filter(getattr(Incident, key).like(f"%{value}%"))
+        query = db.session.query(Incident)
+
+        # Aplicar filtros a Incident
+        query = apply_filters(query, Incident, filters)
+
+        # Filtros para la tabla relacionada Users (customer)
+        if any([customer_id, customer_username, customer_email, customer_name]):
+            query = query.join(Users, Users.id == Incident.customer_id)
+            customer_filters = {
+                "id": customer_id,
+                "username": customer_username,
+                "email": customer_email,
+                "name": customer_name
+            }
+            query = apply_filters(query, Users, customer_filters)        
+
+        # Filtros para la tabla relacionada IncidentLog
+        if any([incident_logs_id, incident_logs_details, incident_logs_created_date]):
+            query = query.join(IncidentLog, IncidentLog.incident_id == Incident.id)
+            incident_log_filters = {
+                "id": incident_logs_id,
+                "details": incident_logs_details,
+                "created_date": incident_logs_created_date
+            }
+            query = apply_filters(query, IncidentLog, incident_log_filters)
+
         return query.all()
+    
 
     @strawberry.field
-    def log_invoices(self, id: Optional[str] = None, amount: Optional[float] = None, id_invoice: Optional[str] = None, created_date: Optional[str] = None, quantity: Optional[int] = None, description: Optional[str] = None, source: Optional[str] = None) -> List[LogInvoicesType]:
-        query = db.session.query(LogInvoices)
+    def incident_logs(self, 
+                      id: Optional[str] = None, 
+                      details: Optional[str] = None, 
+                      created_date: Optional[str] = None, 
+                      incident_id: Optional[str] = None, 
+                      user_id: Optional[str] = None, 
+                      user_username: Optional[str] = None, 
+                      user_email: Optional[str] = None, 
+                      user_name: Optional[str] = None) -> List[IncidentLogType]:
+        # Filtros para IncidentLog
         filters = {
             "id": id,
-            "amount": amount,
-            "id_invoice": id_invoice,
+            "details": details,
             "created_date": created_date,
-            "quantity": quantity,
-            "description": description,
-            "source": source
+            "incident_id": incident_id
         }
-        for key, value in filters.items():
-            if value:
-                query = query.filter(getattr(LogInvoices, key).like(f"%{value}%"))
+        query = db.session.query(IncidentLog)
+        query = apply_filters(query, IncidentLog, filters)
+
+        # Filtros para la tabla relacionada Users (user en logs)
+        if any([user_id, user_username, user_email, user_name]):
+            query = query.join(Users, Users.id == IncidentLog.users_id)
+            user_filters = {
+                "id": user_id,
+                "username": user_username,
+                "email": user_email,
+                "name": user_name
+            }
+            query = apply_filters(query, Users, user_filters)
+        
         return query.all()
 
     @strawberry.field
-    def contracts(self, id: Optional[str] = None, description: Optional[str] = None, start_date: Optional[str] = None, end_date: Optional[str] = None, plan: Optional[str] = None, company_id: Optional[str] = None) -> List[ContractType]:
-        query = db.session.query(Contract)
+    def contracts(self, id: Optional[str] = None, description: Optional[str] = None, start_date: Optional[str] = None, 
+                  end_date: Optional[str] = None, plan: Optional[str] = None, company_id: Optional[str] = None) -> List[ContractType]:
         filters = {
             "id": id,
             "description": description,
@@ -164,27 +215,24 @@ class Query:
             "plan": plan,
             "company_id": company_id
         }
-        for key, value in filters.items():
-            if value:
-                query = query.filter(getattr(Contract, key).like(f"%{value}%"))
+        query = db.session.query(Contract)
+        query = apply_filters(query, Contract, filters)
         return query.all()
 
     @strawberry.field
     def companies(self, id: Optional[str] = None, name: Optional[str] = None, created_date: Optional[str] = None) -> List[CompanyType]:
-        query = db.session.query(Company)
         filters = {
             "id": id,
             "name": name,
             "created_date": created_date
         }
-        for key, value in filters.items():
-            if value:
-                query = query.filter(getattr(Company, key).like(f"%{value}%"))
+        query = db.session.query(Company)
+        query = apply_filters(query, Company, filters)
         return query.all()
 
     @strawberry.field
-    def rates(self, id: Optional[str] = None, rate: Optional[float] = None, rate_per_incident: Optional[float] = None, id_contract: Optional[str] = None, source: Optional[str] = None) -> List[RatesType]:
-        query = db.session.query(Rates)
+    def rates(self, id: Optional[str] = None, rate: Optional[float] = None, rate_per_incident: Optional[float] = None, 
+              id_contract: Optional[str] = None, source: Optional[str] = None) -> List[RatesType]:
         filters = {
             "id": id,
             "rate": rate,
@@ -192,14 +240,13 @@ class Query:
             "id_contract": id_contract,
             "source": source
         }
-        for key, value in filters.items():
-            if value:
-                query = query.filter(getattr(Rates, key).like(f"%{value}%"))
+        query = db.session.query(Rates)
+        query = apply_filters(query, Rates, filters)
         return query.all()
 
     @strawberry.field
-    def invoices(self, id: Optional[str] = None, amount: Optional[float] = None, id_contract: Optional[str] = None, company_id: Optional[str] = None, created_date: Optional[str] = None) -> List[InvoicesType]:
-        query = db.session.query(Invoices)
+    def invoices(self, id: Optional[str] = None, amount: Optional[float] = None, id_contract: Optional[str] = None, 
+                 company_id: Optional[str] = None, created_date: Optional[str] = None) -> List[InvoicesType]:
         filters = {
             "id": id,
             "amount": amount,
@@ -207,36 +254,21 @@ class Query:
             "company_id": company_id,
             "created_date": created_date
         }
-
-    @strawberry.field
-    def log_invoices(self, id: Optional[str] = None, amount: Optional[float] = None, id_invoice: Optional[str] = None, created_date: Optional[str] = None, quantity: Optional[int] = None, description: Optional[str] = None, source: Optional[str] = None) -> List[LogInvoicesType]:
-        query = db.session.query(LogInvoices)
-        filters = {
-            "id": id,
-            "amount": amount,
-            "id_invoice": id_invoice,
-            "created_date": created_date,
-            "quantity": quantity,
-            "description": description,
-            "source": source
-        }
-        for key, value in filters.items():
-            if value:
-                query = query.filter(getattr(LogInvoices, key).like(f"%{value}%"))
+        query = db.session.query(Invoices)
+        query = apply_filters(query, Invoices, filters)
         return query.all()
 
     @strawberry.field
-    def company_services(self, id: Optional[str] = None, description: Optional[str] = None, id_company: Optional[str] = None, created_date: Optional[str] = None) -> List[CompanyServicesType]:
-        query = db.session.query(CompanyServices)
+    def company_services(self, id: Optional[str] = None, description: Optional[str] = None, 
+                         id_company: Optional[str] = None, created_date: Optional[str] = None) -> List[CompanyServicesType]:
         filters = {
             "id": id,
             "description": description,
             "id_company": id_company,
             "created_date": created_date
         }
-        for key, value in filters.items():
-            if value:
-                query = query.filter(getattr(CompanyServices, key).like(f"%{value}%"))
+        query = db.session.query(CompanyServices)
+        query = apply_filters(query, CompanyServices, filters)
         return query.all()
 
 # Definir el esquema de GraphQL
